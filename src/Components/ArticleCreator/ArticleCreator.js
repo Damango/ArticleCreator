4f465e6ea9cf94023b1d5f6ab9ad7bc3db7bfc86
Damango <37273048+Damango@users.noreply.github.com>
2021-03-01 13:01:21 -0500

    Added back buttons for view states. Basic Article View.

--- src/Components/ArticleCreator/ArticleCreator.js
+++ src/Components/ArticleCreator/ArticleCreator.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import "./ArticleCreator.css"
+import articles from "../../data/articles.json"
 const ArticleCreator = (props) => {
     return (<div className="article-creator-container">
 
         <div className="close-button" onClick={props.closeView}>X</div>
         <h1>Article creator</h1>
+
+        <div className="article-setup-container">
+
+
+        </div>
 
 
 
--- src/Components/ArticleReader/ArticleReader.js
+++ src/Components/ArticleReader/ArticleReader.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import articles from "../../data/articles.json"
+import "./ArticleReader.css"
 const ArticleReader = (props) => {
     return (<div className="article-reader-container">
 
         <div className="close-button" onClick={props.closeView}>X</div>
         <h1>Article Reader</h1>
+
+        <div className="article">
+            {articles.articles.map((article) => <div>{article.articleTitle}</div>)}
+        </div>
 
 
     </div>);
