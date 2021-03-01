a284c60a6e9e4b129dac5f612d56b62f628d13c1
Damango <37273048+Damango@users.noreply.github.com>
2021-03-01 13:06:10 -0500

    JSON file changed

--- src/Components/ArticleReader/ArticleReader.js
+++ src/Components/ArticleReader/ArticleReader.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import articles from "../../data/articles.json"
-import "./ArticleReader.css"
+import React from "react";
+import articles from "../../data/articles.json";
+import "./ArticleReader.css";
 const ArticleReader = (props) => {
-    return (<div className="article-reader-container">
+  console.log(articles.articles[0]);
+  return (
+    <div className="article-reader-container">
+      <div className="close-button" onClick={props.closeView}>
+        X
+      </div>
+      <h1>Article Reader</h1>
 
-        <div className="close-button" onClick={props.closeView}>X</div>
-        <h1>Article Reader</h1>
-
-        <div className="article">
-            {articles.articles.map((article) => <div>{article.articleTitle}</div>)}
-        </div>
-
-
-    </div>);
-}
+      <div className="article">
+        {articles.articles.map((article) => (
+          <div>{article.articleTitle}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default ArticleReader;
