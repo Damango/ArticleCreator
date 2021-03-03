7eb873083ae48b570b85cfe1b6b849df73abe275
Damango <37273048+Damango@users.noreply.github.com>
2021-03-02 19:20:19 -0500

    Added article posting and text sections.

--- src/Components/ArticleCreator/ArticleCreator.js
+++ src/Components/ArticleCreator/ArticleCreator.js
@@ -1,16 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./ArticleCreator.css"
+import TextSection from "./TextSection/TextSection"
 import articles from "../../data/articles.json"
 const ArticleCreator = (props) => {
+
+    const [article, setArticle] = useState()
+
+
+
+
+    function createArticle() {
+
+        let textareaText = document.querySelector('.text-section-input').value;
+        let articleTitle = document.querySelector('.title-input').value;
+
+
+
+        let newArticle = {
+            articleTitle: articleTitle,
+            articleSections: [
+                { sectionText: textareaText }
+            ],
+            articleDate: "",
+            aritcleID: 1,
+            articleAuthor: "Justin Kessler"
+        }
+
+        console.log(newArticle);
+        setArticle(newArticle)
+
+
+    }
+
+
+    function articlePreview() {
+        props.articlePreview(2)
+        props.preview(article)
+
+    }
+
+
+
+
     return (<div className="article-creator-container">
 
+        <button className="publish-button" onClick={createArticle}>Publish</button>
+
         <div className="close-button" onClick={props.closeView}>X</div>
-        <h1>Article creator</h1>
+
 
         <div className="article-setup-container">
+            <div className="title-entry-container">
+                <div>Title</div>
+                <input className="title-input" placeholder='Enter Title' />
+            </div>
+
+            <div className="text-sections-wrapper">
+                <TextSection />
+
+            </div>
 
 
+            <button className="article-preview-button" onClick={articlePreview}>Preview Article</button>
         </div>
+
+
 
 
 
--- src/Components/ArticleReader/ArticleReader.js
+++ src/Components/ArticleReader/ArticleReader.js
@@ -2,17 +2,39 @@ import React from 'react';
 import articles from "../../data/articles.json"
 import "./ArticleReader.css"
 const ArticleReader = (props) => {
-    return (<div className="article-reader-container">
-
-        <div className="close-button" onClick={props.closeView}>X</div>
-        <h1>Article Reader</h1>
-
-        <div className="article">
-            {articles.articles.map((article) => <div>{article.articleTitle}</div>)}
-        </div>
 
 
-    </div>);
+    if (props.data != null) {
+        return (<div className="article-reader-container">
+
+            <div className="close-button" onClick={props.closeView}>X</div>
+            <div>Article Reader</div>
+
+            <div className="article">
+                {props.data.articleTitle}
+            </div>
+
+
+        </div>)
+    }
+
+
+
+
+
+    else {
+        return (<div className="article-reader-container">
+
+            <div className="close-button" onClick={props.closeView}>X</div>
+            <div>Article Reader</div>
+
+            <div className="article">
+                {articles.articles.map((article) => <div>{article.articleTitle}</div>)}
+            </div>
+
+
+        </div>)
+    }
 }
 
 export default ArticleReader;
